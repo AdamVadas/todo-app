4f18ceb4fa83220f9d26289b8ed590a2a3d7f6fb
AdamVadas <adam.vadas.w17@gmail.com>
2020-02-02 14:42:34 +0100

    todo entity validation and completion function added

--- src/main/java/com/vadas/entity/Todo.java
+++ src/main/java/com/vadas/entity/Todo.java
@@ -1,6 +1,12 @@
 package com.vadas.entity;
 
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.lang.annotation.Native;
 import java.time.LocalDate;
 
 @Entity
@@ -10,8 +16,15 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty(message = "Task must not be empty!")
+    @Size(min = 10, message = "Minimum required characters: 10")
     private String task;
+
+    @NotNull(message = "Due date is mandatory!")
+    @FutureOrPresent(message = "Due date must be in the present or the future!")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     private boolean isCompleted;
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
--- src/main/java/com/vadas/rest/TodoRest.java
+++ src/main/java/com/vadas/rest/TodoRest.java
@@ -4,12 +4,10 @@ import com.vadas.entity.Todo;
 import com.vadas.service.TodoService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("todo")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -23,6 +21,34 @@ public class TodoRest {
     @POST
     public Response createTodo(Todo todo) {
         todoService.createTodo(todo);
+        return Response.ok(todo).build();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateTodo(Todo todo) {
+        todoService.updateTodo(todo);
+        return Response.ok(todo).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Todo getTodo(@PathParam("id") Long id) {
+        return todoService.findTodoById(id);
+    }
+
+    @Path("list")
+    @GET
+    public List<Todo> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @Path("status")
+    @POST
+    public Response markAsComplete(@QueryParam("id") Long id){
+        Todo todo = todoService.findTodoById(id);
+        todo.setCompleted(true);
+        todoService.updateTodo(todo);
         return Response.ok(todo).build();
     }
 }
