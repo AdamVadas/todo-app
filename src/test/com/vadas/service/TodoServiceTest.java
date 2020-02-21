43e8a331fbd1cb96bcbe48a7dcb99fad099472e9
AdamVadas <adam.vadas.w17@gmail.com>
2020-02-21 15:41:10 +0100

    added logger for tests

--- src/test/com/vadas/service/TodoServiceTest.java
+++ src/test/com/vadas/service/TodoServiceTest.java
@@ -1,30 +1,55 @@
 package com.vadas.service;
 
 import com.vadas.entity.Todo;
+import com.vadas.entity.User;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import javax.inject.Inject;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(Arquillian.class)
 public class TodoServiceTest {
 
+    @Inject
+    private User user;
+
+    @Inject
+    TodoService todoService;
+    Logger logger;
+
+
     @Deployment
     public static JavaArchive createDeployment(){
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClasses(TodoService.class, Todo.class)
+        return ShrinkWrap.create(JavaArchive.class, "todo-app")
+                .addPackage(Todo.class.getPackage())
+                .addPackage(TodoService.class.getPackage())
+                .addAsResource("persistence.xml", "META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Before
     public void setUp() throws Exception {
+
+        logger = Logger.getLogger(TodoServiceTest.class.getName());
+        user.setEmail("bla@bla.com");
+        user.setFullName("John Doe");
+        user.setPassword("blablabla");
+
+        todoService.saveUser(user);
     }
 
     @After
@@ -32,18 +57,11 @@ public class TodoServiceTest {
     }
 
     @Test
-    public void createTodo() {
+    public void saveUser(){
+        assertNotNull(user.getId());
+        logger.log(Level.INFO, user.getId().toString());
+        assertNotEquals("The user password is not the same as the hashed one", "blablabla", user.getPassword());
+        logger.log(Level.INFO, user.getPassword());
     }
 
-    @Test
-    public void updateTodo() {
-    }
-
-    @Test
-    public void findTodoById() {
-    }
-
-    @Test
-    public void getTodos() {
-    }
 }
